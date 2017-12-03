bf0ccd63032af6fa15697fdc8ab441e88e6f6a21
dennismfong <dfong2014@gmail.com>
2017-12-02 22:35:43 -0800

    Add registerUser function to trader interface

--- TraderInterface.java
+++ TraderInterface.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
@@ -7,32 +8,65 @@ public class TraderInterface {
 
 	}
 
+	public void displayGreeting() {
+		System.out.println("Welcome to the Trader Interface of StarsRUs");
+		System.out.println("Issue commands using the number key associated with your request");
+	}
+
 	public boolean login(String username, String password) {
 		boolean success = true;
 		// Add in code to check if user is in the customer table
 		return success;
 	}
 
+	public void registerUser() {
+		System.out.println("Welcome to the register portal of StarsRUs");
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter in your desired username");
+			String username = br.readLine();
+			Console console = System.console();
+			char[] password = console.readPassword("Enter password");
+			System.out.println(password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) {
 		TraderInterface traderifc = new TraderInterface();
 
 		boolean loggedIn = false;
-		System.out.println("Welcome to the Trader Interface of StarsRUs");
-		while (!loggedIn) {
-				try {
-					BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-					System.out.println("Please enter in your username");
-					String username = reader.readLine();
-					System.out.println("Please enter in your password");
-					String password = reader.readLine();
+		traderifc.displayGreeting();
 
-					loggedIn = traderifc.login(username, password);
-					if (!loggedIn) {
-						System.out.println("Sorry, that user was not found, please try again");
-					}
-				} catch (Exception e) {
-					System.err.println(e);
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Are you a registered user?");
+			System.out.println("1. yes");
+			System.out.println("2. no");
+
+			String answer = reader.readLine();
+			if ("2".equals(answer)) {
+				traderifc.registerUser();
+			}
+
+			while (!loggedIn) {
+				System.out.println("Please enter in your username");
+				String username = reader.readLine();
+				System.out.println("Please enter in your password");
+				String password = reader.readLine();
+
+				loggedIn = traderifc.login(username, password);
+				if (!loggedIn) {
+					System.out.println("Sorry, that user was not found, please try again");
 				}
+				else {
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
