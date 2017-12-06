df849ff3f13a078dff75ad125b6cf8681e4417d1
dennismfong <dfong2014@gmail.com>
2017-12-05 21:06:15 -0800

    Changed runit script to .java extensions for autocomplete

--- runit.sh
+++ runit.sh
@@ -1,2 +1,7 @@
+string=$1
+if [[ $string == *.java ]]; then
+v2=${string%.*}
+fi
+echo $v2
 javac *.java
-java -cp /cs/class/cs174a/public/mysql-connector-java-5.1.44-bin.jar:. $1
+java -cp /cs/class/cs174a/public/mysql-connector-java-5.1.44-bin.jar:. $v2
