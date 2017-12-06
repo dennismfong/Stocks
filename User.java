ebdbc0125240e39a3b702e21b4cd84b37ac905a7
dennismfong <dfong2014@gmail.com>
2017-12-05 16:27:08 -0800

    Changed User methods to non static

--- User.java
+++ User.java
@@ -3,21 +3,21 @@ public class User{
 	private float balance;
 	private String name;
 
-	public static void setName(String name){
+	public void setName(String name){
 		this.name = name;
 	}
 
-	public static void setBalance(float balance){
+	public void setBalance(float balance){
 		this.balance = balance;
 	}
 
-	public static void setUsername(String username){
+	public void setUsername(String username){
 		this.username = username;
 	}
 
-	public static float getBalance(){ return this.balance; }
+	public float getBalance(){ return this.balance; }
 
-	public static String getName(){ return this.name; }
+	public String getName(){ return this.name; }
 
-	public static String getUsername(){ return this.username; }
+	public String getUsername(){ return this.username; }
 }
