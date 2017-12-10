47c315f483edc92cb223dd8b25d5dbae07232355
dennismfong <dfong2014@gmail.com>
2017-12-09 22:09:58 -0800

    Generate monthly statement with interest to do

--- ManagerInterface.java
+++ ManagerInterface.java
@@ -1,3 +1,4 @@
+import javax.xml.transform.Result;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.*;
@@ -109,6 +110,11 @@ public class ManagerInterface {
           case 4:
             break;
           case 5:
+            System.out.println("\nEnter username of customer");
+            String username2 = reader.readLine();
+            managerifc.generateCustomerReport(username2);
+            break;
+          case 6:
             break;
           case 7:
             exitPortal = true;
@@ -158,8 +164,6 @@ public class ManagerInterface {
       temp = c.getTime();
       java.sql.Date lastOfMonth = new java.sql.Date(temp.getTime());
 
-      //String query_3 = "select balance from BalanceHistory where aid = ? and MONTH(date) = MONTH(?) and YEAR(date) = YEAR(?)" +
-      //        " and (DAY(date) = DAY(?) or DAY(date) = DAY(?))";
       String query_3 = "select balance from BalanceHistory where aid = ? and (date = ? or date = ?)";
       PreparedStatement dateStatement = connection.prepareStatement(query_3);
       dateStatement.setInt(1, aid);
@@ -171,12 +175,37 @@ public class ManagerInterface {
       resultSet_3.next();
       double endBalance = resultSet_3.getDouble(1);
 
+      // Getting the net earnings and net losses
+      String transactionQuery = "select * from Transaction t join StockTransaction s on t.tid = s.tid where" +
+              " date >= ? and date <= ?";
+      PreparedStatement netStatement = connection.prepareStatement(transactionQuery);
+      netStatement.setDate(1, firstOfMonth);
+      netStatement.setDate(2, lastOfMonth);
+      ResultSet resultSet_4 = netStatement.executeQuery();
+      double earnings = 0;
+      double losses = 0;
+      double interest = 0;
+      while (resultSet_4.next()) {
+        if (resultSet_4.getString(2).toLowerCase().contains("stock bought")) {
+          losses += resultSet_4.getDouble(9);
+        }
+        else if (resultSet_4.getString(2).toLowerCase().contains("stock sold")) {
+          earnings += resultSet_4.getDouble(9);
+        }
+        else if (resultSet_4.getString(2).toLowerCase().contains("interest")) {
+          interest += resultSet_4.getDouble(9);
+        }
+      }
 
 			System.out.println("MONTHLY STATEMENT FOR: " + cname + " (" + email + ")\n");
       System.out.println("Starting Balance:   " + startBalance);
-      System.out.println("End Balance:        " + endBalance + "\n");
+      System.out.println("End Balance:        " + endBalance);
+      System.out.println("Total earnings:     " + earnings + interest);
+      System.out.println("  from stocks: "      + earnings);
+      System.out.println("  from interest: "    + interest);
+      System.out.println("Total losses:       " + losses + "\n");
       while (resultSet_2.next()) {
-				System.out.println(resultSet_2.getString(1) + "|" + resultSet_2.getString(2) + "|" + resultSet_2.getDate(3) + "|" + resultSet_2.getString(4));
+				System.out.println(resultSet_2.getString(1) + " | " + resultSet_2.getString(2) + " | " + resultSet_2.getDate(3) + " | " + resultSet_2.getString(4));
 			}
       statement.close();
 			connection.close();
